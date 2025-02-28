40868eeafb8a3b08f4c3b142379a44569c676e62
amare1990 <amaremek@gmail.com>
2025-02-28 16:08:15 +0300

    Create stock_forcasting.py script and StockForecasting class initialized with processed data

---
+++ scripts/stock_forcasting.py
@@ -0,0 +1,12 @@
+"""Time-series forcasting using statistical and ML models."""
+
+
+
+# Define the StockForecasting class
+class StockForecasting:
+    def __init__(self, processed_data):
+        self.data = processed_data
+
+
+
+
